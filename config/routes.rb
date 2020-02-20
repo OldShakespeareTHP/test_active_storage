4009b380ad7b7f8caea68e1905563258fa2fc1de
forejy.joffrey <joffrey.marques@gmail.com>
2020-02-20 17:36:09 +0100

    pour test sur heroku

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'users/show'
+  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :avatars, only: [:create]
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
